4849524719f1c99774b17a4bc52c181c11103f98
ahmadzendi <72287812+ahmadzendi@users.noreply.github.com>
2025-08-21 17:56:45 +0700

    Update web.py

--- web.py
+++ web.py
@@ -140,4 +140,6 @@ def data():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
